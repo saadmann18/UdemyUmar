bd4fe19e3ba4e59465eaebdd46f1782d7c83bc7a
saadmann18 <saadmann18@gmail.com>
2025-01-03 23:52:37 +0100

    insertion operator overloaded

--- OperatorOverloading/Main.cpp
+++ OperatorOverloading/Main.cpp
@@ -13,16 +13,20 @@ std::ostream & operator <<(std::ostream& out, const Integer& a)
 	out << a.GetValue();
 	return out;
 }
+std::istream& operator >>(std::istream& input, Integer& a)
+{
+	int x;
+	input >> x;
+	a.SetValue(x);
+	return input;
+}
 
 int main()
 {
 	Integer a(4), b(4);
 	
-	Integer sum1 = b + 5;
-	Integer sum2 = 5 + b; //Global function overload needed.
-
-	std::cout << sum1 << std::endl;
-	std::cout << sum2 << std::endl;
+	std::cin >> a;
+	std::cout << a << std::endl;
 
 
 	
