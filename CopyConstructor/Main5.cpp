d66cb3b87fdb515e518781e730fddce1ad926a55
saadmann18 <saadmann18@gmail.com>
2025-01-03 22:10:43 +0100

    CopyConstructor Project file added

---
+++ CopyConstructor/Main5.cpp
@@ -0,0 +1,34 @@
+//Main5.cpp is to implement the Rule of 5
+#include "Integer.h"
+class Number
+{
+	Integer m_Value{};
+public:
+	Number() = default;
+	Number(int value) :m_Value{ value }
+	{
+	}
+	//Number(const Number &n):m_Value{n.m_Value}{}
+
+	Number(Number&& n) = default;
+	Number(const Number& n) = default;
+
+	Number& operator=(Number&&) = default;
+	Number& operator=(const Number&) = default;
+
+};
+Number CreateNumber(int num)
+{
+	Number n{ num };
+	return n;
+}
+
+int main()
+{
+	Number n1; //Number n{1};
+	auto n2{ n1 };
+	n2 = n1;
+
+	auto n3{ CreateNumber(3) };
+	n3 = CreateNumber(3);
+}
