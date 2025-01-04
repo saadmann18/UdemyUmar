21ce2ec37aadba62e25bfafee36cd9ec01b259ff
saadmann18 <saadmann18@gmail.com>
2025-01-04 01:09:56 +0100

    SmartPointer Project created

--- SmartPointers/Integer.cpp
+++ SmartPointers/Integer.cpp
@@ -104,3 +104,22 @@ Integer::~Integer()
 	delete m_pInt;
 }
 
+Integer operator +(int x, const Integer& y)
+{
+	Integer temp;
+	temp.SetValue(x + y.GetValue());
+	return temp;
+}
+
+std::ostream& operator <<(std::ostream& out, const Integer& a)
+{
+	out << a.GetValue();
+	return out;
+}
+std::istream& operator >>(std::istream& input, Integer& a)
+{
+	int x;
+	input >> x;
+	*a.m_pInt = x;
+	return input;
+}
--- SmartPointers/Integer.h
+++ SmartPointers/Integer.h
@@ -48,22 +48,3 @@ class Printer
 	// friend classes and friend functions should be used only as a last resort to solve problems.
 };
 
-Integer operator +(int x, const Integer& y)
-{
-	Integer temp;
-	temp.SetValue(x + y.GetValue());
-	return temp;
-}
-
-std::ostream& operator <<(std::ostream& out, const Integer& a)
-{
-	out << a.GetValue();
-	return out;
-}
-std::istream& operator >>(std::istream& input, Integer& a)
-{
-	int x;
-	input >> x;
-	*a.m_pInt = x;
-	return input;
-}
