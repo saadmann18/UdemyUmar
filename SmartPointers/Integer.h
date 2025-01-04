5478d6b97b44753b312e446cf973b4b371090e12
saadmann18 <saadmann18@gmail.com>
2025-01-04 01:09:36 +0100

    SmartPointer Project created

---
+++ SmartPointers/Integer.h
@@ -0,0 +1,69 @@
+#pragma once
+class Integer
+{
+	int* m_pInt;
+public:
+	//Default constructor
+	Integer();
+
+	//Parametrized constructor
+	Integer(int value);
+
+	//copy constructor for deep copy
+	Integer(const Integer &obj);
+
+	//Move constructor for shallow copy
+	Integer(Integer &&obj);
+
+	//copy assignment
+	Integer& operator=(const Integer& obj);
+
+	//Move assignment
+	Integer& operator=( Integer&& obj);
+
+	int GetValue() const;
+	void SetValue(int value);
+
+	Integer operator+(const Integer& a) const; //doesnot modifiy the state of the object, so 'const' at end;
+	Integer& operator++();
+	Integer operator++(int);
+
+	bool operator ==(const Integer& obj) const;
+
+	//function call operator to print contents of the object
+	void operator ()();
+
+	friend std::istream& operator >>(std::istream& input, Integer& a);
+	friend class Printer;
+
+	~Integer(); 
+
+};
+
+class Printer
+{
+	//Printer class wants to access the private members	of Integer, declaring friend of Integer.
+	// Discouraged because it allows to access the internal data of the class directly.
+	// Can be a source of bugs.
+	// friend classes and friend functions should be used only as a last resort to solve problems.
+};
+
+Integer operator +(int x, const Integer& y)
+{
+	Integer temp;
+	temp.SetValue(x + y.GetValue());
+	return temp;
+}
+
+std::ostream& operator <<(std::ostream& out, const Integer& a)
+{
+	out << a.GetValue();
+	return out;
+}
+std::istream& operator >>(std::istream& input, Integer& a)
+{
+	int x;
+	input >> x;
+	*a.m_pInt = x;
+	return input;
+}
---
+++ SmartPointers/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "Integer.h"
+
+
+
+int main()
+{
+
+	return 0;
+}
