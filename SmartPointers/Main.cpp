2096307e1df16656ff5664966ae13791bff52cbe
saadmann18 <saadmann18@gmail.com>
2025-01-04 01:14:44 +0100

    CreateInteger function created in the heap

--- SmartPointers/Main.cpp
+++ SmartPointers/Main.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 #include "Integer.h"
 
-
+void CreateInteger()
+{
+	Integer* p = new Integer;
+	p->SetValue(3);
+	std::cout << p->GetValue() << std::endl;
+	delete p;
+}
 
 
 int main()
 {
-	
+	CreateInteger();
 
 	return 0;
 }
