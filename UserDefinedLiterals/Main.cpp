02625a35ebfcd0c7d8ba29c8576f0e96a89ce861
saadmann18 <saadmann18@gmail.com>
2025-01-05 22:13:58 +0100

    UserDefinedLiterals project added. overloaded functions added.

---
+++ UserDefinedLiterals/Main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+class Distance
+{
+	long double m_Kilometers;
+public:
+	Distance(long double km): m_Kilometers{km}{}
+	long double GetKm()const {
+		return m_Kilometers;
+	}
+	void setKm(long double val) {
+		m_Kilometers = val;
+	}
+};
+Distance operator""_mi(long double val) {
+	return Distance{ val * 1.6 };
+}
+Distance operator""_meters(long double val) {
+	return Distance{ val / 1000 };
+}
+int main() {
+	Distance dist{ 32.0_mi };
+	Distance d2{ 7234.0_meters };
+	std::cout << d2.GetKm() << std::endl;
+	return 0;
+}
