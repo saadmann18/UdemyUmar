781343ad02c79001da3e32f9cffd66ec6e70e85a
saadmann18 <saadmann18@gmail.com>
2025-01-05 13:30:46 +0100

    problem with manually assigning values

---
+++ Enums/Main.cpp
@@ -0,0 +1,19 @@
+#define RED 0
+const int GREEN = 1;
+void FillColor(int color)
+{
+	if(color == 0){
+		//Paint with red color
+	}
+	else if (color == 1){
+		//paint with green color
+	}
+}
+
+int main() {
+	FillColor(RED);
+	FillColor(GREEN);
+	FillColor(6);
+
+	return 0;
+}
