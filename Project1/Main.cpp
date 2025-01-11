c30576516c65e8bbddc8fcc60eb0a03a8234d707
saadmann18 <saadmann18@gmail.com>
2025-01-11 21:13:07 +0100

    Savings class works but it does not invoke GetMinimumBalance()

--- Project1/Main.cpp
+++ Project1/Main.cpp
@@ -7,22 +7,7 @@
 
 int main()
 {
-	Checking ch("Saad", 100, 0.05f);
-	//Transact(&ch);
-	Account* p = &ch;
-
-	int i{};
-	float f{}; 
-	const std::type_info& ti = typeid(*p);
-	std::cout << ti.name() << std::endl;
-
-	if (ti == typeid(Savings))
-	{
-		std::cout << "Pointing to a Savings object" << std::endl;
-	}
-	else
-	{
-		std::cout << "not Pointing to a Savings object" << std::endl;
-	}
+	Savings ch("Saad", 100, 0.05f);
+	Transact(&ch);
 	return 0;
 }
