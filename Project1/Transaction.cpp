5cd0920972f83d087aa2845a5e912526e5c9d068
saadmann18 <saadmann18@gmail.com>
2025-01-11 21:16:18 +0100

    dynamic_cast<> added to the Transaction Checking

--- Project1/Transaction.cpp
+++ Project1/Transaction.cpp
@@ -8,12 +8,16 @@ void Transact(Account* pAccount)
 	std::cout << "Initial balance: " << pAccount->GetBalance() << std::endl;
 	pAccount->Diposit(100);
 	pAccount->AccumulateInterest();
-	if (typeid(*pAccount) == typeid(Checking))
+	/*if (typeid(*pAccount) == typeid(Checking))
 	{
 		Checking* pChecking = static_cast<Checking*>(pAccount);
 		std::cout << "Minimum balance of Checking: " << pChecking->GetMinimumBalance() << std::endl;
-	}
+	}*/
 	
+	Checking* pChecking = dynamic_cast<Checking*>(pAccount);
+	if (pChecking != nullptr) {
+		std::cout << "Minimum balance of Checking: " << pChecking->GetMinimumBalance() << std::endl;
+	}
 
 	pAccount->Withdraw(170);
 	std::cout << "Interest rate: " << pAccount->GetInterestRate() << std::endl;
