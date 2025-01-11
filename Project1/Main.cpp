edcd41c3a295e0b689cad4bb5f0d701d0ebdc2d5
saadmann18 <saadmann18@gmail.com>
2025-01-11 13:07:36 +0100

    typeinfo header overview

--- Project1/Main.cpp
+++ Project1/Main.cpp
@@ -3,10 +3,18 @@
 #include "Savings.h"
 #include "Checking.h"
 #include "Transaction.h"
+#include<typeinfo>
 
 int main()
 {
-	Checking ch("Saad", 100, 50);
-	std::cout << ch.GetMinimumBalance();
+	Savings ch("Saad", 100, 0.05f);
+	//Transact(&ch);
+	Account* p = &ch;
+
+	int i{};
+	float f{};
+	const std::type_info& ti = typeid(*p);
+	std::cout << ti.name() << std::endl;
+	 
 	return 0;
 }
