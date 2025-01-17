69ada2278e69cdf797cb562cc5ac20ed614a202d
saadmann18 <saadmann18@gmail.com>
2025-01-17 19:25:50 +0100

    explicit specialization template for vector type added

--- ClassTemplateExplicitSpecialization/Main.cpp
+++ ClassTemplateExplicitSpecialization/Main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 template <typename T>
 class PrettyPrint {
@@ -27,6 +28,22 @@ public:
 	}
 };
 
+template<>
+class PrettyPrint<std::vector<int>> {
+	std::vector<int>* m_pData;
+public:
+	PrettyPrint(std::vector<int>* data) : m_pData(data) {}
+	void Print() {
+		std::cout << "{";
+		for (const auto& x : *m_pData) {
+			std::cout << x;
+		}
+		std::cout << "}" << std::endl;
+	}
+	std::vector<int>* GetData() {
+		return m_pData;
+	}
+};
 int main() {
 	/*int data = 5;
 	float f = 8.5;
@@ -35,10 +52,14 @@ int main() {
 	PrettyPrint<float> p2(&f);
 	p2.Print();*/
 
-	char* p{ "Hello World" };
+	/*char* p{"Hello World"};
 	PrettyPrint<char*> p3(p);
 	p3.Print();
-	char* pData = p3.GetData();
+	char* pData = p3.GetData();*/
+
+	std::vector<int>v{ 1,2,3,4,5 };
+	PrettyPrint<std::vector<int>> pv(&v);
+	pv.Print();
 
 	return 0;
 }
