644441a066015bdc0094eec8cabf84b5391c2c67
saadmann18 <saadmann18@gmail.com>
2025-01-17 19:10:35 +0100

    explicit specialization template for char type added

--- ClassTemplateExplicitSpecialization/Main.cpp
+++ ClassTemplateExplicitSpecialization/Main.cpp
@@ -13,11 +13,32 @@ public:
 	}
 };
 
+
+template<>
+class PrettyPrint<char*> {
+	char* m_pData;
+public:
+	PrettyPrint(char *data): m_pData(data){}
+	void Print() {
+		std::cout << "{" << m_pData << "}" << std::endl;
+	}
+	char* GetData() {
+		return m_pData;
+	}
+};
+
 int main() {
-	int data = 5;
+	/*int data = 5;
 	float f = 8.5;
 	PrettyPrint<int> p1(&data);
 	p1.Print();
 	PrettyPrint<float> p2(&f);
-	p2.Print();
+	p2.Print();*/
+
+	char* p{ "Hello World" };
+	PrettyPrint<char*> p3(p);
+	p3.Print();
+	char* pData = p3.GetData();
+
+	return 0;
 }
