d4f7f4f9f6311959282c0b7ea443a1bda3139b66
saadmann18 <saadmann18@gmail.com>
2025-01-28 05:49:38 +0100

    Unordered set functionality added

---
+++ Hash/Main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <unordered_set>
+#include <string>
+
+void UnSet() {
+	std::unordered_set<std::string> coll;
+	coll.insert("Hulk");
+	coll.insert("Batman");
+	coll.insert("Green Lantern");
+	coll.insert("The Flash");
+	coll.insert("Wonder Woman");
+	coll.insert("Iron man");
+	coll.insert("Woverine");
+	coll.insert("Dr. Strange");
+	coll.insert("Hawkman");
+
+	for (const auto x : coll) {
+		std::cout << "Bucket #: " << coll.bucket(x) << " Contains :" << x << std::endl;
+	}
+	std::cout << "Bucket count: " << coll.bucket_count() << std::endl;
+	std::cout << "Number of elements: " << coll.size() << std::endl;
+	std::cout << "Load factor: " << coll.load_factor() << std::endl;
+}
+
+void Hashes() {
+	std::hash<std::string> h;
+	std::cout << "Hash:" << h("Hello") << std::endl;
+}
+
+int main() {
+	UnSet();
+	return 0;
+}
