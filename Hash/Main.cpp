d38de7f8494806fd11c175c446adfd4460614314
saadmann18 <saadmann18@gmail.com>
2025-01-29 05:03:32 +0100

    function objects for hashing and equitiy is added

--- Hash/Main.cpp
+++ Hash/Main.cpp
@@ -41,12 +41,45 @@ void UnMap() {
 	}
 }
 
+class Employee {
+	std::string m_Name;
+	int m_Id;
+public:
+	Employee(const std::string &n, int id):m_Name(n), m_Id(id){}
+	const std::string& GetName()const {
+		return m_Name;
+	}
+	int GetId()const {
+		return m_Id;
+	}
+};
+
+struct EmployeeHash {
+	size_t operator()(const Employee& emp)const {
+		auto s1 = std::hash<std::string>{}(emp.GetName());
+		auto s2 = std::hash<int>{}(emp.GetId());
+		return s1 ^ s2;
+	}
+};
+struct EmpEquality {
+	bool operator()(const Employee& e1, const Employee& e2)const {
+		return e1.GetId() == e2.GetId() && e1.GetName() == e2.GetName();
+	}
+};
 void Hashes() {
 	std::hash<std::string> h;
 	std::cout << "Hash:" << h("Hello") << std::endl;
+	std::unordered_set<Employee, EmployeeHash, EmpEquality> coll;
+	coll.insert(Employee{ "Saad", 123 });
+	coll.insert(Employee{ "Joye", 456 });
+	coll.insert(Employee{ "bob", 678 });
+
+	for (const auto& x : coll) {
+		std::cout << x.GetId() << ':' << x.GetName() << '\n';
+	}
 }
 
 int main() {
-	UnMap();
+	Hashes();
 	return 0;
 }
