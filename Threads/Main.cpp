41f4db3cad6f2672b92abf880dd8cb20a2016078
saadmann18 <saadmann18@gmail.com>
2025-02-01 09:20:25 +0100

    std::ref() is used to pass argument by ref, ensuring 1 instance of String class is created

--- Threads/Main.cpp
+++ Threads/Main.cpp
@@ -3,19 +3,44 @@
 #include <thread>
 
 std::list<int> g_Data;
-const int SIZE = 50000000;
+const int SIZE = 5000000;
 
-void Download() {
-	std::cout << "Started" << std::endl;
+class String {
+public:
+	//default constructor
+	String() {
+		std::cout << "String()" << std::endl;
+	}
+	//copy constructor
+	String(const String &) {
+		std::cout << "String(const String&)" << std::endl;
+	}
+	//Assignment operator
+	String &operator=(const String&) {
+		std::cout << "operator=(const String&) " << std::endl;
+		return *this;
+	}
+	//Destructor
+	~String() {
+		std::cout << "~String()" << std::endl;
+	}
+};
+
+void Download(String &file) {
+	//std::cout << "Started downloading: " << file << std::endl;
 	for (int i = 0; i < SIZE; ++i) {
 		g_Data.push_back(i);
 	}
 	std::cout << "Finished" << std::endl;
 }
 int main() {
+	String file;
 	std::cout << "[main] started" << std::endl;
-	std::thread thDownloader(Download);
-	std::cout << "[main] started another" << std::endl;
-	thDownloader.join();
+	std::thread thDownloader(Download, std::ref(file));
+	//thDownloader.detach();
+	std::cout << "[main] continued operation" << std::endl;
+	if (thDownloader.joinable()) {
+		thDownloader.join();
+	}
 	return 0;
 }
