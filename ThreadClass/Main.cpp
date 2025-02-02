d402068cc5353ac307e881c647fe7d11dc0d0349
saadmann18 <saadmann18@gmail.com>
2025-02-02 07:44:08 +0100

    SetThreadDescription to set thread name for identification

--- ThreadClass/Main.cpp
+++ ThreadClass/Main.cpp
@@ -8,6 +8,8 @@ void Process() {
 
 int main() {
 	std::thread t1(Process);
+	HANDLE	handle = t1.native_handle();
+	SetThreadDescription(handle, L"MyThread");
 	auto id = t1.get_id();
 	std::cout << id << std::endl;
 	t1.native_handle();
